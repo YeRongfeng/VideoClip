b2cd9613281841cc8167475d824da31187b1c688
YeRongfeng <202300171211@mail.sdu.edu.cn>
2025-07-13 14:12:53 +0800

    chore: 更新项目配置文件，优化依赖管理和元数据

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ import os
 import tkinter as tk
 from tkinter import filedialog, messagebox
 
-from config import *
+from config import (
+    CONTROL_BG,
+    DEFAULT_OUTPUT_EXTENSION,
+    OUTPUT_VIDEO_FORMATS,
+    SUPPORTED_VIDEO_FORMATS,
+    WINDOW_BG,
+    WINDOW_SIZE,
+    WINDOW_TITLE,
+)
 from crop_controller import CropController
 from ui_components import StatusBar, VideoCanvas, VideoControlPanel
 from utils import format_time, generate_output_filename
